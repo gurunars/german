ce50680896a0df7ba06eca133f99f76840ca4b33
Anton Berezin <gurunars@gmail.com>
2023-02-26 22:15:50 +0100

    ff

--- grammar/Program.fs
+++ grammar/Program.fs
@@ -1,2 +1,11 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
